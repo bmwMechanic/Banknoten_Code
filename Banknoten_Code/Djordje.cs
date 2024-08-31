74c9d0ab78829c3f469514554f07a1012b724e84
DJ <d.tepic@icloud.com>
2024-08-31 22:30:10 +0200

    started w/ only change methods and properties to private, stopped w/ a bit of code improvement. interesting: : base()

--- Banknoten_Code/Djordje.cs
+++ Banknoten_Code/Djordje.cs
@@ -2,38 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Banknoten_Code
 {
     public  class Djordje
     {
-        public List<string> codeList = new List<string>();
-        public List<int> intList = new List<int>();
+        private List<string> codeList = new List<string>(); //why does private work here?
+        private List<int> intList = new List<int>();        //why does private work here?
 
-        public int digitsum = 0;
-        public int checksum = 0;
+        //protected int digitsum { get; private set; }
+        //protected int checksum { get;  private set; }
+        private int digitsum { get; set; }  //getter & seeter are private by default?
+        private int checksum { get; set; }  //getter & seeter are private by default?
+        private string eingabe { get; set; }
+        private bool b { get; set; }
         //public string faelschung = ("Die Prüfsumme betraegt {0}\nAchtung: gefaelschte Banknote!", checksum);//chechsum needed to be static to be able to do this.. but then it's not working...
 
-        public Djordje(string eingabe)
+        public Djordje(string Eing) : base()    //ok thanks David      https://stackoverflow.com/questions/63957152/error-cs7036-there-is-no-argument-given-that-corresponds-to-the-required-formal
+        {
+            // "samo da instancira polja"; polja su linije 17-20? kako to onda da uspem?
+            //jer ne mogu ovako ovde u konstrukteru:       private bool b { get; set; }
+            // pitao sam chatgpt sta ja instanciranje polja... a ovde sam dodao polje eingabe koje mogu inteligantno
+            //instancirati s konstruktorom. I vide mi ne treba Parameter za SaveInList();
+            eingabe = Eing;
+        }
+
+        ~ Djordje()
+        {
+            Console.WriteLine("vidimo se :)");  //destr. is enabled in Windows Forms upon closing the window; not here?
+        }
+
+        public string getBanknoteResult()
         {
             //all needed methods;
-            SaveInList(eingabe);
-            bool b = obEingabePasst();   // wenn unsinnige Eingabe, dann Fehlermeldung und return ist fail :D
+            SaveInList();
+            b = obEingabePasst();  
+            //ContinueProgram();
+            return ContinueProgram() + ChecksumComparison();    //nice, works
+        }
+
+        private string ContinueProgram()   //put if in a surrounding method that will conain more of them... :)
+        {
             if (!b)
             {
-                CWriteFail();   //Console.WriteLine("Fail");
+                return FailReturn();   //Console.WriteLine("Fail");
             }
             else
             {
                 BuildIntList();
                 CalcDigitSum(); //look above p int digitsum; no digitsum = calcdigitsum needed
                 checksum = BerechneJ();
+                return null;    // "" is possible as well
             }
+
+            //(!b) ? Method1() : Method2(); NOT POSSIBLE!
         }
-
-
-        public void SaveInList(string eingabe)
+        public void SaveInList()
         {
             //Buchstabe in Zahl und an Stelle 0 in codeList
             foreach (char x in eingabe)
@@ -41,11 +67,11 @@ namespace Banknoten_Code
                 codeList.Add(x.ToString());
             }
         }
-        public void CWriteFail()
+        private string FailReturn()
         {
-            Console.WriteLine("Fail: ungueltiger Code. Siehe Doku.");
+            return "Fail: ungueltiger Code. Siehe Doku.";   //CW weg! für get...result
         }
-        public void BuildIntList()
+        private void BuildIntList()
         {
             codeList[0].ToUpper();
             //codeList[0] = wandleBuchstabeInZahl(codeList[0]); //not possible!
@@ -56,29 +82,33 @@ namespace Banknoten_Code
                 intList.Add(int.Parse(codeList[i]));
             }
         }
-        public void CalcDigitSum()
+        private void CalcDigitSum()
         {
             digitsum = intList.Sum(); //intList ist mit dem umgewandelten LänderCode!
         }
-        public string ChecksumComparison()
+        public string ChecksumComparison()  //bc this is being called from within class Program, it needs to be public. But Encapsulation here?
         {
-            if (int.Parse(codeList[11]) != checksum)    //12.Stelle des Codes! (L)(10xN)(J)
+            if (b)  //dass die Ausgabe Prüfsumme... ausgegeben wird, wenn falscher code eingegeben wurde. Test (falsche UND (zu kurze ODER zu lange) string-eingabe erfolgreich
             {
-                return $"Die Pruefsumme betraegt {checksum}\ngefaelschte Banknote!";
+                if (int.Parse(codeList[11]) != checksum)    //12.Stelle des Codes! (L)(10xN)(J)
+                {
+                    return $"Die Pruefsumme betraegt {checksum}\ngefaelschte Banknote!";
+                }
+                else
+                {
+                    return $"Die Pruefsumme betraegt {checksum}\nOriginale Banknote!";
+                }
             }
-            else
-            {
-                return $"Die Pruefsumme betraegt {checksum}\nOriginale Banknote!";
-            }
+            else return "";
         }
-        public int wandleBuchstabeInZahl()
+        private int wandleBuchstabeInZahl()
         {
             //char 'A' hat einen INT-WErt von 65 bei int.Parse('A');! um 1? --> -64! wie mache ich char.ToUpper()?
             //int.Parse geht nicht bei Chars also --> 
             int zahlMit65zuViel = (int)(Convert.ToChar(codeList[0].ToUpper()));
             return zahlMit65zuViel - 64;
         }
-        public int BerechneJ()
+        private int BerechneJ() //J is checksum
         {
             int rest = digitsum % 9;
             if (8 - rest == 0)
@@ -90,22 +120,25 @@ namespace Banknoten_Code
                 return 8 - rest;
             }
         }
-        public bool obEingabePasst()
+        private bool obEingabePasst()   //logic is not too beautiful but works; cannot put for
         {
-            bool b = true;
-            for (int i = 1; i < codeList.Count; i++)
-            {
-                if (codeList[i] != "0" && codeList[i] != "1" && codeList[i] != "2" && codeList[i] != "3" && codeList[i] != "4" && codeList[i] != "5" && codeList[i] != "6" && codeList[i] != "7" && codeList[i] != "8" && codeList[i] != "9")
-                {
-                    b = false;  //ist hier nötig!
-                    break;
-                }
-            }
+            string pattern = @"^[0-9]$";
+            Regex reg = new Regex(pattern);
             if (codeList.Count != 12)
             {
-                b = false;
+                return false;
             }
-            return b;
+            else
+            {
+                for (int i = 1; i < codeList.Count; i++)
+                {
+                    if (!reg.IsMatch(codeList[i]))  //reg.IsMatch returns BOOL, so if(!BOOL) means if bool = false  :) 
+                    {
+                        return false;  //ist hier nötig!
+                    }
+                }
+            }
+            return true;
         }
     }
 }
--- Banknoten_Code/Program.cs
+++ Banknoten_Code/Program.cs
@@ -1,17 +1,25 @@
-﻿namespace Banknoten_Code
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace Banknoten_Code
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //idea: divide into more classes intelligently to reply in various languages. sounds fun right?
+            Console.Write("Enter Code like this: ^[A-Za-z]\\d(11)$\nInsert Code Here: ");
             string eingabe = Console.ReadLine();//first string var
             //then object creation w/ contructor
             Djordje dj = new Djordje(eingabe);
             //Console.WriteLine(Djordje dj = new Djordje(eingabe));    ---not possible---
 
-            Console.WriteLine(dj.ChecksumComparison());
-
+            Console.WriteLine(dj.getBanknoteResult());  //returns string only; now w/o CW in methods exception's calling....
             Console.ReadKey();
+            dj = null;  //'non-nullable type'? 
+            GC.Collect();   //Destructor seems not to be working..? or Garbage is not collected...?
+            Console.ReadKey();
+
         }
     }
 }
